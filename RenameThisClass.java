e618ae00d8e8884071116c3b846b6dddb0e5c73a
Hudson Land <hudson.land@mrjaffesclass.com>
2023-09-13 11:10:56 -0700

    W me

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,5 +9,13 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int i = 10;
+        float f = 2.5f;
+        boolean flag = true;
+        f += i;
+        flag = !flag;
+        i += 1;
+        System.out.print (i++);
+        System.out.print (++f);
     }
 }
