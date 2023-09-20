dab690f30fe598abbf70e1ef6d40bce6fa8d36fb
Hudson Land <hudson.land@mrjaffesclass.com>
2023-09-20 11:39:59 -0700

    Arrays

--- Arrays.java
+++ Arrays.java
@@ -5,19 +5,25 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class RenameThisClass
+public class Arrays
 {
     public static void main(String[] args) {
         String[] strArray = {"Alpha", "Bravo", "Charlie", "Delta", "Echo"};
+
+        System.out.println ("\n");
         for (int i = 0; i < strArray.length; i++) {
             System.out.println(strArray[i]);    
         }
-        for (int i = 0; i < strArray.length; i++) {
-            System.out.print(strArray[i]);
+
+        System.out.println ("\n");
+        for (String item : strArray) {
+            System.out.print(item + " ");
         }    
+
+        System.out.println ("\n");
         for (int i = 0; i < strArray.length; i++) {
-            System.out.print(strArray[i]);
-            System.out.print("-");
+            System.out.print(strArray[i] + "-");
         }
-    }
+        
+    } 
 } 
